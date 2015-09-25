1f1c15592736219743dcff040c4b66824776f5c6
bkraul <bkraul@yahoo.com>
2015-09-25 16:25:59 -0500

    Corrections to previewer (some items were not being displayed correctly)

--- BBCodePlus/pages/preview.php
+++ BBCodePlus/pages/preview.php
@@ -5,28 +5,10 @@ html_head_begin();
 html_content_type();
 html_css();
 html_head_javascript();
-
-// load bbcode style.
-$resources = '<link rel="stylesheet" type="text/css" href="' . plugin_file("bbcodeplus.css") . '" />';
-$resources .= '<script type="text/javascript" src="' . plugin_file( 'bbcodeplus-init.js' ) . '"></script>';
-if ( ON == plugin_config_get( 'process_highlight' ) ) {
-	// load highlighting if turned on.
-	$resources .= '<link rel="stylesheet" type="text/css" href="' . plugin_file("bbcodeplus.css") . '" />';		
-	$resources .= '<link rel="stylesheet" type="text/css" href="' . plugin_file( 'prism/styles/' . plugin_config_get( 'highlight_css' ) . '.css' ) . '" />';
-	$resources .= '<script type="text/javascript" src="' . plugin_file( 'prism/prism.js' ) . '"></script>';			
-	
-	// load additional languages.
-	if ( ON == plugin_config_get( 'highlight_extralangs' ) ) {
-		$resources .= '<script type="text/javascript" src="' . plugin_file( 'prism/prism_additional_languages.js' ) . '"></script>';		
-	}
-}
-
-// output resources.
-echo $resources;
+html_head_end();
 ?>
-</head>
-<body>
-	<div class="bbcodeplus-preview"><?php echo string_display_links($_POST["data"]); ?></div>
+<body class="bbcodeplus-preview-body">
+    <div class="bbcodeplus-preview"><?php echo string_display_links($_POST["data"]); ?></div>
 </body>
 <?php
 html_end();
