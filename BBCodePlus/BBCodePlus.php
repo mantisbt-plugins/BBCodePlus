01fb084047f0ac9e1198cd32ecefd9ca325508d9
bkraul <bkraul@yahoo.com>
2015-09-29 16:03:13 -0500

    Updated to 1.0.6.

--- BBCodePlus/BBCodePlus.php
+++ BBCodePlus/BBCodePlus.php
@@ -19,7 +19,7 @@
 			$this->name        = plugin_lang_get( 'title' );
 			$this->description = plugin_lang_get( 'description' );
 			$this->page        = 'config';
-			$this->version     = '1.0.5';
+			$this->version     = '1.0.6';
 			
 			$this->requires['MantisCore'] = '1.2.0';
 			// require jQuery.
