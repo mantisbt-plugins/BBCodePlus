31bbc34f2083759b947cd50dc00282ccc9c03ec9
bkraul <bkraul@yahoo.com>
2015-09-29 08:52:20 -0500

    Added [highlight] tag and color selector. Corrected styling of preview panel. Eliminated php-css streaming in lieu of plugin_file.php urls.

--- BBCodePlus/files/bbcodeplus-init.js
+++ BBCodePlus/files/bbcodeplus-init.js
@@ -10,12 +10,27 @@
 				var matches = /^bbcolor\-([^\s]+)/.exec($(obj).attr('class'));
 				// get the color.
 				color = matches[1];
-				// set the element color
+				// set the element's color
 				// (this is accepted by CSP guidelines per http://www.cspplayground.com/compliant_examples#style)
 				$(obj).css('color', color);
 			});
 		}
 
+		// scan bbhighlight classes.
+		var highlightElements = $("span[class*='bbhighlight-']");
+		if (highlightElements != null) {
+			// iterate through the elements and extract the size from the class name.
+			highlightElements.each(function(index, obj) {
+				// regex matches.
+				var matches = /^bbhighlight\-([^\s]+)/.exec($(obj).attr('class'));
+				// get the color.
+				color = matches[1];
+				// set the element's background color.
+				// (this is accepted by CSP guidelines per http://www.cspplayground.com/compliant_examples#style)
+				$(obj).css('background-color', color);
+			});			
+		}
+		
 		// scan bbsize classes.
 		var sizeElements = $("span[class*='bbsize-']");
 		if (sizeElements != null) {
@@ -25,7 +40,7 @@
 				var matches = /^bbsize\-([^\s]+)/.exec($(obj).attr('class'));
 				// get the color.
 				size = matches[1];
-				// set the element size
+				// set the element's size
 				// (this is accepted by CSP guidelines per http://www.cspplayground.com/compliant_examples#style)
 				$(obj).css('font-size', size + '%');
 			});			
