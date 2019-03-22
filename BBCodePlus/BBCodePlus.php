5f226ab1e12a353f3810c991554f83e728cfdfbd
bkraul <bkraul@yahoo.com>
2019-03-22 05:58:14 -0500

    Bumped version to 2.1.3 and updated README

--- BBCodePlus/BBCodePlus.php
+++ BBCodePlus/BBCodePlus.php
@@ -23,7 +23,7 @@
          $this->name        = plugin_lang_get( 'title' );
          $this->description = plugin_lang_get( 'description' );
          $this->page        = 'config';
-         $this->version     = '2.1.2';
+         $this->version     = '2.1.3';
 
          $this->requires['MantisCore'] = '2.0.0';
          # this plugin can coexist with MantisCoreFormatting.
