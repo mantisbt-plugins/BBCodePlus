9e8c4628ef9467e0647e9fa868069f540a1f3b14
Christian Schulz <christian.schulz@fsd-web.de>
2019-10-21 13:06:34 +0200

    Fixed display of quotes when quotee's name contains unicode characters, commas or single quotes

--- BBCodePlus/BBCodePlus.php
+++ BBCodePlus/BBCodePlus.php
@@ -278,7 +278,7 @@
          $this->t_bbCode->addParser('code-lang-ln', '/\[code=(\w*)\ start=([0-9]+)\](.*?)\[\/code\]/s',
                                     '<pre class="bbcodeplus pre line-numbers" data-start="$2"><code class="bbcodeplus code language-$1">$3</code></pre>', '$3');
          $this->t_bbCode->addParser('quote', '/\[quote\](.*?)\[\/quote\]/s', '<blockquote class="bbcodeplus blockquote">$1</blockquote>', '$1');
-         $this->t_bbCode->addParser('named-quote', '/\[quote=([\s\w]*)\](.*?)\[\/quote\]/s',
+         $this->t_bbCode->addParser('named-quote', '/\[quote=([\s\w,\']*)\](.*?)\[\/quote\]/su',
                                     '<blockquote class="bbcodeplus blockquote"><p class="mb-0">$2</p><footer class="bbcodeblus blockquote-footer"><cite title="$1">$1</cite></footer></blockquote>',
                                     '$1 wrote: $2');
       }
