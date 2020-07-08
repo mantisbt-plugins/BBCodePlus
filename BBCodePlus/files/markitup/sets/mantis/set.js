1d7c08fc24a9bf067e34581b72e36ac0e0b60d7a
bkraul <bkraul@yahoo.com>
2020-07-08 16:07:57 +0000

    Removed unused function on set.js

--- BBCodePlus/files/markitup/sets/mantis/set.js
+++ BBCodePlus/files/markitup/sets/mantis/set.js
@@ -256,15 +256,3 @@ mySettings = {
         {name:'Preview', className:'preview',  call:'preview'},
     ]
 }
-
-function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split('&');
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        if (decodeURIComponent(pair[0]) == variable) {
-            return decodeURIComponent(pair[1]);
-        }
-    }
-    console.log('Query variable %s not found', variable);
-}
